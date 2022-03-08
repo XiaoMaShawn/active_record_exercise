1db8b81c1a3c0773917e19395e0853e292c64463
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-03-07 19:30:58 -0500

    connect stores table with employees table and insert data for employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store4 = Store.find_by(id: 4)
+@store5 = Store.find_by(id: 5)
+@store6 = Store.find_by(id: 6)
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Leo", last_name: "Jaz", hourly_rate: 60)
+@store2.employees.create(first_name: "Jason", last_name: "Wang", hourly_rate: 34)
+@store2.employees.create(first_name: "Tim", last_name: "Le", hourly_rate: 34)
+@store4.employees.create(first_name: "Tom", last_name: "Edson", hourly_rate: 56)
+@store5.employees.create(first_name: "Jack", last_name: "Mark", hourly_rate: 70)
+@store6.employees.create(first_name: "Susi", last_name: "Elina", hourly_rate: 90)
+
