2829cde8b2a2d98c492878f4b012d682fa6cb554
XiaoMaShawn <ma2014xiao@hotmail.com>
2022-03-07 20:48:34 -0500

    add validates and show the error message to user

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -14,9 +14,9 @@ puts "----------"
 @store6 = Store.find_by(id: 6)
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "Leo", last_name: "Jaz", hourly_rate: 60)
-@store2.employees.create(first_name: "Jason", last_name: "Wang", hourly_rate: 34)
-@store2.employees.create(first_name: "Tim", last_name: "Le", hourly_rate: 34)
+@store1.employees.create(first_name: "Leo", last_name: "Jaz", hourly_rate: 65)
+@store2.employees.create(first_name: "Jason", last_name: "Wang", hourly_rate: 44)
+@store2.employees.create(first_name: "Tim", last_name: "Le", hourly_rate: 54)
 @store4.employees.create(first_name: "Tom", last_name: "Edson", hourly_rate: 56)
 @store5.employees.create(first_name: "Jack", last_name: "Mark", hourly_rate: 70)
 @store6.employees.create(first_name: "Susi", last_name: "Elina", hourly_rate: 90)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "please enter a store name: "
+input_name = $stdin.gets.chomp 
+
+puts input_name
+
+new_store = Store.create(name: "#{input_name}")
+
+new_store.errors.messages.each { |message| 
+  # print message
+  puts message
+}
+# p new_store.errors
+# puts new_store.errors.messages
